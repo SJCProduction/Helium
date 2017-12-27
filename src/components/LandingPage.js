54154b6efcd287d7156bd69eeb3102dcf8e99f32
Julie Moon <8juliemoon@gmail.com>
2017-12-27 12:55:16 -0500

    layout change to have logo in between text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,10 +3,11 @@ import './LandingPage.css'
 const LandingPage = () =>{
   return(
     <React.Fragment> 
+    <h3>Helium.js is a node package that helps make your React application isomorphic and optimized</h3>
     <div className="logoDiv">
-    <img className ="logo" src="../src/images/helium.png" alt="heliumLogo"/>
+    <img className ="logo" src="../src/images/heliumjs.png" alt="heliumLogo"/>
     </div>
-    <h1> Welcome to Helium.js</h1>
+    <p>What is the benefit of server-side rendering?<br/>Leveraging server-side rendering can significantly improve first page load performance: render JavaScript templates on the server to deliver fast first render, and then use client-side templating once the page is loaded. However, performance benefits depend on the use case and server-side rendering is not a one size fits all design.</p>
     </React.Fragment>
   )
 }
