83418cf490c9b9ecaee4f033191509d1f9d9ca99
Julie Moon <8juliemoon@gmail.com>
2017-12-31 14:18:26 -0500

    add Julie info

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const ContactUs = () => {
   return (
-      <h1>Get to know us</h1>
+    <React.Fragment>
+      <h1>Get to know us:</h1>
+      <div className="wrapper">
+      <p className="J">Julie</p>
+      <p className="Jabout">I think React is nifty, I j'adore CSS Grid, and sometimes I feel like learning takes forever and a day, but honestly the payoff is pretty rad. View my <a href="https://www.linkedin.com/in/julie-moon/">  LinkedIn</a> page.</p>
+      <p className="C">Chris</p>
+      <p className="Cabout">C content </p>
+      <p className="S">Shachy</p>
+      <p className="Sabout">S content</p>
+      </div>
+      </React.Fragment>
   )
 }
 
