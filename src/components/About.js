6a2bec1f5cd8994912b231c01a355001afcca947
Julie Moon <8juliemoon@gmail.com>
2017-12-31 14:17:39 -0500

    add working links and general outline of Helium

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const About = ()=> {
-  return <div>This is the About Page</div>
+  return (
+  <React.Fragment>
+    <p className="about">Currently, Helium.js includes server side rendering with support for React Router v4 and Redux v3 using React Fiber - v16. </p>
+    <p className="about"><strong>Coming soon</strong>: Optimization for webpack bundles!</p>
+    <p>For detailed documentation on usage, please visit our github page <a href="https://github.com/SJCProduction/Helium.js"> <i class="fab fa-github fa-2x"></i></a> or <a href="https://www.npmjs.com/package/helium.js">npm</a>.</p>
+
+  </React.Fragment>
+  )
 }
 
 export default About;
