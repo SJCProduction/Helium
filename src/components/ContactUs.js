16fb743f12fb934dd75863f8c96e991eb2a1e4be
Julie Moon <8juliemoon@gmail.com>
2018-01-02 09:55:20 -0500

    add LinkedIn links

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,11 +6,11 @@ const ContactUs = () => {
       <h1>Get to know us:</h1>
       <div className="wrapper">
       <p className="J">Julie</p>
-      <p className="Jabout">I think React is nifty, I j'adore CSS Grid, and sometimes I feel like learning takes forever and a day, but honestly the payoff is pretty rad. View my <a href="https://www.linkedin.com/in/julie-moon/">  LinkedIn</a> page.</p>
+      <p className="Jabout">I think React and JavaScript are pretty nifty. I have a love-hate relationship with CSS. Sometimes, it seems learning takes forever and a day, but the payoff is pretty rad. View my <a href="https://www.linkedin.com/in/julie-moon/">  LinkedIn</a> page.</p>
       <p className="C">Chris</p>
-      <p className="Cabout">C content </p>
+      <p className="Cabout"> View my <a href="https://www.linkedin.com/in/chrisdigitiali/">  LinkedIn</a> page. </p>
       <p className="S">Shachy</p>
-      <p className="Sabout">S content</p>
+      <p className="Sabout">View my <a href="https://www.linkedin.com/in/shachyjr/">  LinkedIn</a> page.</p>
       </div>
       </React.Fragment>
   )
